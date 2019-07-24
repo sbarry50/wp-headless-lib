0dd140eaeda204ee4d366d4e26eb661aa122d488
Steve <sbarry50@gmail.com>
2019-07-24 09:59:43 -0400

    Add plugin dependencies check to Compatibility

--- src/Setup/Compatibility.php
+++ src/Setup/Compatibility.php
@@ -12,7 +12,6 @@
 namespace SB2Media\Headless\Setup;
 
 use SB2Media\Headless\Application;
-use SB2Media\Headless\File\Loader;
 use SB2Media\Headless\Events\EventManager;
 
 class Compatibility
@@ -62,57 +61,70 @@ class Compatibility
     public $min_php_version = '7.0';
 
     /**
+     * Any plugins that are dependencies for this plugin
+     *
+     * @var string|array
+     */
+    public $dependencies;
+
+    /**
      * Constructor
      *
      * @since 0.1.0
      */
-    public function __construct(Application $app)
+    public function __construct(Application $app, $dependencies = null)
     {
         $this->app = $app;
         $this->plugin_name = $app->name;
         $this->wp_version = get_bloginfo('version');
         $this->php_version = phpversion();
-        $this->check();
+        $this->dependencies = $dependencies;
+        $this->checkCompatibility();
+        $this->checkDependencies();
     }
 
     /**
-     * Check if requirements are met to activate and run plugin
+     * Render the "Requirements not met" error notice
      *
      * @since  0.1.0
-     * @return null
+     * @return void
      */
-    public function check()
+    public function renderCompatibilityNotice()
     {
-        if ($this->allCompatible()) {
-            return;
-        } else {
-            $this->addAdminEvents();
-        }
+        $this->app->get('views')->render('errors/compatibility-notice', $this);
     }
 
     /**
-     * Check if all requirements are met
+     * Render the dependent plugins not active error notice
      *
-     * @since  0.1.0
-     * @return bool
+     * @since  0.3.8
+     * @param  $dependency    The plugin dependency configuration
+     * @return void
      */
-    public function allCompatible()
+    public function renderDependencyNotice(array $dependency)
     {
-        return $this->isCompatible($this->wp_version, $this->min_wp_version) &&
-               $this->isCompatible($this->php_version, $this->min_php_version);
+        $args = [
+            'plugin_name' => $this->app->name,
+            'dependency_name' => $dependency['name'],
+            'dependency_src' => $dependency['src']
+        ];
+
+        $this->app->get('views')->render('errors/dependency-notice', $args);
     }
 
     /**
-     * Check if specific requirement is met
+     * Render the dashicon in the "Requirements not met" error notice
      *
      * @since  0.1.0
      * @param  string    $current Current version
      * @param  string    $minimum Minimum required version
-     * @return bool
+     * @return null
      */
-    public function isCompatible($current, $minimum)
+    public function renderDashicon($current, $minimum)
     {
-        return version_compare($current, $minimum, '>=');
+        $this->isCompatible($current, $minimum) ? ($dashicon = 'yes' and $color = '#46b450') : ($dashicon = 'no' and $color = '#dc3232');
+
+        printf('<span class="dashicons dashicons-%s" style="color:%s"></span>', $dashicon, $color);
     }
 
     /**
@@ -134,39 +146,79 @@ class Compatibility
     }
 
     /**
-     * Render the "Requirements not met" error notice
+     * Check if requirements are met to activate and run plugin
      *
      * @since  0.1.0
      * @return null
      */
-    public function renderNotice()
+    private function checkCompatibility()
     {
-        $this->app->get('views')->render('errors/compatibility-notice', $this);
+        if ($this->allCompatible()) {
+            return;
+        }
+
+        if (!$this->allCompatible()) {
+            EventManager::addAction('admin_init', [$this, 'disablePlugin']);
+            EventManager::addAction('admin_notices', [$this, 'renderCompatibilityNotice']);
+        }
     }
 
     /**
-     * Render the dashicon in the "Requirements not met" error notice
+     * Check to see if all plugin dependencies are active
+     *
+     * @since 0.3.8
+     * @return void
+     */
+    private function checkDependencies()
+    {
+        if (is_null($this->dependencies)) {
+            return;
+        }
+
+        foreach ($this->dependencies as $dependency) {
+            if (!$this->dependencyActive($dependency['path'])) {
+                EventManager::addAction('admin_init', [$this, 'disablePlugin']);
+                EventManager::addAction('admin_notices', function () use ($dependency) {
+                    $this->renderDependencyNotice($dependency);
+                });
+            }
+        }
+    }
+
+    /**
+     * Check if all requirements are met
+     *
+     * @since  0.1.0
+     * @return bool
+     */
+    private function allCompatible()
+    {
+        return $this->isCompatible($this->wp_version, $this->min_wp_version) &&
+               $this->isCompatible($this->php_version, $this->min_php_version);
+    }
+
+    /**
+     * Check if specific requirement is met
      *
      * @since  0.1.0
      * @param  string    $current Current version
      * @param  string    $minimum Minimum required version
-     * @return null
+     * @return bool
      */
-    public function renderDashicon($current, $minimum)
+    private function isCompatible($current, $minimum)
     {
-        $this->isCompatible($current, $minimum) ? ($dashicon = 'yes' and $color = '#46b450') : ($dashicon = 'no' and $color = '#dc3232');
-
-        printf('<span class="dashicons dashicons-%s" style="color:%s"></span>', $dashicon, $color);
+        return version_compare($current, $minimum, '>=');
     }
 
     /**
-     * Add admin event listeners
+     * Check if any dependent plugins are active
      *
-     * @since 0.1.0
+     * @since 0.3.8
+     * @param  string   $dependency_path    The path to the dependency
+     * @return boolean
      */
-    private function addAdminEvents()
+    private function dependencyActive(string $dependency_path)
     {
-        EventManager::addAction('admin_init', [$this, 'disablePlugin']);
-        EventManager::addAction('admin_notices', [$this, 'renderNotice']);
+        return in_array($dependency_path, apply_filters('active_plugins', get_option('active_plugins')));
     }
 }
